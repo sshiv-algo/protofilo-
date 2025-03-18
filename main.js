c819220a41a542d4a5c3002657addb1befe1f68b
shivadev sunil <ssh.shivadev@protonmail.com>
2025-03-18 10:52:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-    });
+const nav = document.getElementById("nav");
+let lastScrollY = window.scrollY;
+let menu = document.getElementById("menu");
+let menuIcon = document.getElementById("menu-icon");
+let closeBtn = document.getElementById("closeBtn");
+let responsiveNav = document.getElementById("responsive-nav");
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav-hidden");
+    nav.style.background = "none";
+  } else if (lastScrollY > window.scrollY) {
+    nav.classList.remove("nav-hidden");
+    nav.classList.add("new-nav");
+  }
+  
+  if (lastScrollY < 100  ) {
+    nav.classList.remove("new-nav");
+    nav.style.background = "none";
+  }
+  else {
+    nav.style.background = " #ffffff07";
+  }
+
+
+  
+
+ console.log(lastScrollY) ;
+  lastScrollY = window.scrollY;
 });
+
+function menuOpenFn(){
+  menu.style.display="block";
+  menuIcon.style.display="none";
+}
+
+function menuCloseFn(){
+  menu.style.display="none";
+  menuIcon.style.display="block";
+  
+}
+
